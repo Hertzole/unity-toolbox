00fe0498081c32777946dfaf4bb990513199ab54
Hertzole <hampusringstrom@gmail.com>
2023-01-15 16:15:59 +0100

    add: gizmo color scope

---
+++ Packages/se.hertzole.unitytoolbox/Runtime/Helpers/GizmoColorScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+	public readonly struct GizmoColorScope : IDisposable
+	{
+		private readonly Color originalColor;
+		
+		public GizmoColorScope(Color color)
+		{
+			originalColor = Gizmos.color;
+			Gizmos.color = color;
+		}
+
+		public void Dispose()
+		{
+			Gizmos.color = originalColor;
+		}
+	}
+}
