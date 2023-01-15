6133eca212ecbaa21fd12b81f40e0cdbcb62d2b3
Hertzole <hampusringstrom@gmail.com>
2023-01-15 14:53:41 +0100

    add: input components

---
+++ Packages/se.hertzole.unitytoolbox/Runtime/Scriptable Values/ScriptablePlayerInputsList.cs
@@ -0,0 +1,13 @@
+﻿#if TOOLBOX_INPUT_SYSTEM && TOOLBOX_SCRIPTABLE_VALUES
+using AuroraPunks.ScriptableValues;
+using Hertzole.UnityToolbox.Interfaces;
+using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+#if UNITY_EDITOR
+	[CreateAssetMenu(fileName = "New Player Inputs List", menuName = "Toolbox/Scriptable Values/Collections/Player Inputs List", order = -1000)]
+#endif
+	public sealed class ScriptablePlayerInputsList : ScriptableList<IHasPlayerInput> { }
+}
+#endif
