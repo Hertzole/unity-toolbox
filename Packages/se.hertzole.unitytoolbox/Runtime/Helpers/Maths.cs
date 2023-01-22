45d2b0d138074527109fd0315a18e86c50e7b93a
Hertzole <hampusringstrom@gmail.com>
2023-01-22 14:41:10 +0100

    add: maths helper

---
+++ Packages/se.hertzole.unitytoolbox/Runtime/Helpers/Maths.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Hertzole.UnityToolbox
+{
+	public static class Maths
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int RollOver(int value, int max)
+		{
+			if (value < 0)
+			{
+				return max - 1;
+			}
+
+			if (value >= max)
+			{
+				return 0;
+			}
+
+			return value;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int RollOver(int value, int min, int max)
+		{
+			if (value < min)
+			{
+				return max - 1;
+			}
+
+			if (value >= max)
+			{
+				return min;
+			}
+
+			return value;
+		}
+	}
+}
