d825c4fcec0aa2ec9618f9a2b567831afe665b4e
Hertzole <hampusringstrom@gmail.com>
2023-01-24 19:37:25 +0100

    add: log success

---
+++ Runtime/Helpers/LoggerHelper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+	internal static class LoggerHelper
+	{
+		public static void LogSuccess(object message, Object context = null)
+		{
+			if (context == null)
+			{
+				Debug.Log($"<color=green>{message}</color>");
+			}
+			else
+			{
+				Debug.Log($"<color=green>{message}</color>", context);
+			}
+		}
+		
+		public static void LogSuccessFormat(object message, Object context = null, params object[] args)
+		{
+			if (context == null)
+			{
+				Debug.LogFormat($"<color=green>{message}</color>", args);
+			}
+			else
+			{
+				Debug.LogFormat(context, $"<color=green>{message}</color>", args);
+			}
+		}
+	}
+}
