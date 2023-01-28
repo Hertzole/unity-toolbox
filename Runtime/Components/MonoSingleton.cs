91aaf6109ade6a5352d302f50e415cdf6dce94ac
Hertzole <hampusringstrom@gmail.com>
2023-01-28 14:38:24 +0100

    add: mono singleton

---
+++ Runtime/Components/MonoSingleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+	public abstract class MonoSingleton<T> : MonoBehaviour where T : Object
+	{
+		private static T singletonInstance;
+
+		public static T Instance
+		{
+			get
+			{
+				if (singletonInstance == null)
+				{
+					singletonInstance = FindObjectOfType<T>();
+				}
+
+				return singletonInstance;
+			}
+		}
+		
+		protected virtual bool KeepAlive { get; } = true;
+
+		protected void Awake()
+		{
+			if (singletonInstance != null && singletonInstance != this)
+			{
+				Destroy(gameObject);
+				return;
+			}
+
+			singletonInstance = (T) (object) this;
+			
+			if (KeepAlive)
+			{
+				DontDestroyOnLoad(gameObject);
+			}
+		}
+
+		protected virtual void OnAwake() { }
+	}
+}
