b213eedce7703edc1f0a5e2e67e65dd4f3f8d09e
Hertzole <hampusringstrom@gmail.com>
2023-02-11 22:14:52 +0100

    add: layer and tag type

---
+++ Editor/Elements/ToolboxPropertyDrawer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Hertzole.UnityToolbox.Editor
+{
+	/// <summary>
+	///     Base class for toolbox property drawers. This class handles the tooltip and alignment of the property.
+	/// </summary>
+	public abstract class ToolboxPropertyDrawer : PropertyDrawer
+	{
+		protected virtual bool HandleTooltip { get { return true; } }
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			DrawGUI(position, property, label);
+		}
+
+		public override VisualElement CreatePropertyGUI(SerializedProperty property)
+		{
+			VisualElement field = CreateGUI(property);
+
+			field.RegisterCallback<AttachToPanelEvent, VisualElement>((evt, args) =>
+			{
+				if (args.parent is PropertyField)
+				{
+					args.AddToClassList(BaseField<object>.alignedFieldUssClassName);
+				}
+			}, field);
+
+			if (HandleTooltip && !string.IsNullOrEmpty(property.tooltip))
+			{
+				field.tooltip = property.tooltip;
+			}
+
+			return field;
+		}
+
+		protected abstract void DrawGUI(Rect position, SerializedProperty property, GUIContent label);
+		
+		protected abstract VisualElement CreateGUI(SerializedProperty property);
+	}
+}
---
+++ Runtime/Datatypes/Layer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+	/// <summary>
+	///     Helper type for easily selecting a layer in the inspector.
+	/// </summary>
+	[Serializable]
+	public struct Layer : IEquatable<Layer>, IEquatable<int>
+	{
+		[SerializeField]
+		private int value;
+
+		public static implicit operator int(Layer layer)
+		{
+			return layer.value;
+		}
+
+		public static implicit operator Layer(int layer)
+		{
+			return new Layer { value = layer };
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is Layer other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return value;
+		}
+
+		public static bool operator ==(Layer left, Layer right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(Layer left, Layer right)
+		{
+			return !left.Equals(right);
+		}
+
+		public bool Equals(int other)
+		{
+			return value == other;
+		}
+
+		public bool Equals(Layer other)
+		{
+			return value == other.value;
+		}
+	}
+}
