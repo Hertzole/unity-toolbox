375d2978dee77e146e2e00a5d8134d1726951095
Hertzole <hampusringstrom@gmail.com>
2023-08-04 16:31:08 +0200

    add: damageable and healable interfaces

---
+++ Packages/se.hertzole.unitytoolbox/Runtime/Interfaces/IHealable.cs
@@ -0,0 +1,11 @@
+﻿namespace Hertzole.UnityToolbox
+{
+	public interface IHealable
+	{
+#if TOOLBOX_PHYSICS_2D || TOOLBOX_PHYSICS_3D
+		void Heal(int amount, HitData hitData = default, IDamager healer = null);
+#else
+		void Heal(int amount, IDamager healer = null);
+#endif
+	}
+}
