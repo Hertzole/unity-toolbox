1d2ec00a23f521817555bec45290de4e586b945d
Hertzole <hampusringstrom@gmail.com>
2023-08-04 16:26:07 +0200

    add: scan on start for navmesh

---
+++ Packages/se.hertzole.unitytoolbox/Runtime/Components/ScanOnStart.cs
@@ -0,0 +1,37 @@
+﻿#if TOOLBOX_NAVIGATION
+using Unity.AI.Navigation;
+using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+#if UNITY_EDITOR
+	[RequireComponent(typeof(NavMeshSurface))]
+#endif
+	public sealed class ScanOnStart : MonoBehaviour
+#if UNITY_EDITOR
+		, ISerializationCallbackReceiver
+#endif
+	{
+		[SerializeField]
+		[HideInInspector]
+		private NavMeshSurface surface = null;
+
+		private void Start()
+		{
+			surface.BuildNavMesh();
+		}
+
+#if UNITY_EDITOR
+		void ISerializationCallbackReceiver.OnBeforeSerialize()
+		{
+			if (surface == null)
+			{
+				surface = GetComponent<NavMeshSurface>();
+			}
+		}
+
+		void ISerializationCallbackReceiver.OnAfterDeserialize() { }
+#endif
+	}
+}
+#endif
