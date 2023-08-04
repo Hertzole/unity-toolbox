eced59d6f5a3e880e89c6aabfbdcba98208c6423
Hertzole <hampusringstrom@gmail.com>
2023-08-04 16:23:02 +0200

    add: generator

---
+++ Runtime/Attributes/GenerateSubscribeMethodsAttribute.cs
@@ -0,0 +1,9 @@
+﻿#if TOOLBOX_SCRIPTABLE_VALUES
+using System;
+
+namespace Hertzole.UnityToolbox
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public sealed class GenerateSubscribeMethodsAttribute : Attribute { }
+}
+#endif
