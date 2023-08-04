7506568b638ee8e65155df0da17340d37cc2b23f
Hertzole <hampusringstrom@gmail.com>
2023-08-05 00:35:07 +0200

    version it up

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,3 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("1.0.0")]
 
 [assembly: InternalsVisibleTo("Hertzole.UnityToolbox.Editor")]
