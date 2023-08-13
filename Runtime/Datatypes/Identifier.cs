f1cc93022135ca5305c3e12ee08e134585a15083
Hertzole <hampusringstrom@gmail.com>
2023-08-13 17:53:44 +0200

    feat: identifier type

---
+++ Runtime/Datatypes/Identifier.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+	[Serializable]
+	public struct Identifier : IEquatable<Identifier>,
+		IEquatable<int>
+#if UNITY_EDITOR
+		,
+		ISerializationCallbackReceiver
+#endif
+	{
+#if UNITY_EDITOR
+		[SerializeField]
+		private string stringValue;
+#endif
+		[SerializeField]
+		private int value;
+
+		public Identifier(string stringValue)
+		{
+#if UNITY_EDITOR
+			this.stringValue = stringValue;
+#endif
+
+			value = ToStableHash(stringValue);
+		}
+
+		public override int GetHashCode()
+		{
+			return value;
+		}
+
+		public static bool operator ==(Identifier left, Identifier right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(Identifier left, Identifier right)
+		{
+			return !left.Equals(right);
+		}
+
+		private static int ToStableHash(string stringValue)
+		{
+			if (string.IsNullOrEmpty(stringValue))
+			{
+				return 0;
+			}
+			
+			unchecked
+			{
+				int hash = 23;
+				for (int i = 0; i < stringValue.Length; i++)
+				{
+					hash = hash * 31 + stringValue[i];
+				}
+
+				return hash;
+			}
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is Identifier other && Equals(other);
+		}
+
+		public bool Equals(Identifier other)
+		{
+			return value == other.value;
+		}
+
+		public bool Equals(int other)
+		{
+			return value == other;
+		}
+
+#if UNITY_EDITOR
+		void ISerializationCallbackReceiver.OnBeforeSerialize()
+		{
+			value = ToStableHash(stringValue);
+		}
+
+		void ISerializationCallbackReceiver.OnAfterDeserialize()
+		{
+			// Does nothing.
+		}
+#endif
+	}
+}
---
+++ Runtime/Scriptable Values/ScriptableIdentifier.cs
@@ -0,0 +1,12 @@
+﻿#if TOOLBOX_SCRIPTABLE_VALUES
+using Hertzole.ScriptableValues;
+using UnityEngine;
+
+namespace Hertzole.UnityToolbox
+{
+#if UNITY_EDITOR
+	[CreateAssetMenu(fileName = "New Scriptable Identifier", menuName = "Toolbox/Scriptable Values/Values/Scriptable Identifier")]
+#endif
+	public sealed class ScriptableIdentifier : ScriptableValue<Identifier> { }
+}
+#endif
