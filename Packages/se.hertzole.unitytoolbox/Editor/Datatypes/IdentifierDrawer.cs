9649c58e939a53b4bd8d521ed2c42885eaebe8e6
Hertzole <hampusringstrom@gmail.com>
2023-08-13 17:53:44 +0200

    feat: identifier type

---
+++ Packages/se.hertzole.unitytoolbox/Editor/Datatypes/IdentifierDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Hertzole.UnityToolbox.Editor
+{
+	[CustomPropertyDrawer(typeof(Identifier))]
+	public sealed class IdentifierDrawer : ToolboxPropertyDrawer
+	{
+		protected override void DrawGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			SerializedProperty prop = property.FindPropertyRelative("stringValue");
+            prop.stringValue = EditorGUI.TextField(position, label, prop.stringValue);
+		}
+
+		protected override VisualElement CreateGUI(SerializedProperty property, string label)
+		{
+			SerializedProperty prop = property.FindPropertyRelative("stringValue");
+			TextField field = new TextField(label);
+			field.BindProperty(prop);
+
+			return field;
+		}
+	}
+}
