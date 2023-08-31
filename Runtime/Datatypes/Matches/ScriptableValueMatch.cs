ce2c428ea9773b16a964920b5bf95a9da9a07520
Hertzole <hampusringstrom@gmail.com>
2023-08-31 21:55:04 +0200

    feat: addressables support for cursor manager

--- Runtime/Datatypes/Matches/ScriptableValueMatch.cs
+++ Runtime/Datatypes/Matches/ScriptableValueMatch.cs
@@ -3,16 +3,20 @@ using System;
 using Hertzole.ScriptableValues;
 using Hertzole.ScriptableValues.Helpers;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Hertzole.UnityToolbox.Matches
 {
 	[Serializable]
-	public abstract class ScriptableValueMatch<T> : IScriptableMatch
+	public abstract partial class ScriptableValueMatch<T> : IScriptableMatch
 	{
 		[SerializeField]
 		private ScriptableValue<T> target = default;
 		[SerializeField]
 		private T mustMatchValue = default;
+
+		[NonSerialized]
+		private bool hasInitialized;
 
 		public event Action OnValueChanged;
 
@@ -21,20 +25,63 @@ namespace Hertzole.UnityToolbox.Matches
 			OnValueChanged?.Invoke();
 		}
 
+#if TOOLBOX_ADDRESSABLES
+		partial void OnTargetLoaded(ScriptableValue<T> value)
+		{
+			if (hasInitialized)
+			{
+				value.OnValueChanged += OnValueChangedInternal;
+			}
+		}
+#endif
+
 		public void Initialize()
 		{
-			target.OnValueChanged += OnValueChangedInternal;
+			if (hasInitialized)
+			{
+				return;
+			}
+
+#if TOOLBOX_ADDRESSABLES
+			if (useAddressables)
+			{
+				LoadAssets();
+			}
+#endif
+			if (target != null)
+			{
+				target.OnValueChanged += OnValueChangedInternal;
+			}
+
+			hasInitialized = true;
 		}
 
 		public void Dispose()
 		{
-			target.OnValueChanged -= OnValueChangedInternal;
+			if (target != null)
+			{
+				target.OnValueChanged -= OnValueChangedInternal;
+			}
+
+#if TOOLBOX_ADDRESSABLES
+			ReleaseAssets();
+			target = null;
+#endif
+
+			hasInitialized = false;
 		}
 
 		public bool Matches()
 		{
 			return EqualityHelper.Equals(target.Value, mustMatchValue);
 		}
+#if TOOLBOX_ADDRESSABLES
+		[SerializeField]
+		private bool useAddressables = default;
+		[SerializeField]
+		[GenerateLoad]
+		private AssetReferenceT<ScriptableValue<T>> targetReference = default;
+#endif
 	}
 }
 #endif
