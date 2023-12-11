53d4c236d5011aef643be421d43a6307a8dcb8fa
Hertzole <hampusringstrom@gmail.com>
2023-12-11 23:27:25 +0100

    feat: odin validator support for input manager

---
+++ Packages/se.hertzole.unitytoolbox/Editor/Components/ToolboxEditor.cs
@@ -0,0 +1,22 @@
+﻿#if ODIN_INSPECTOR
+using BaseScript = Sirenix.OdinInspector.Editor.OdinEditor;
+
+#else
+using BaseScript = UnityEditor.Editor;
+#endif
+
+namespace Hertzole.UnityToolbox.Editor
+{
+	public abstract class ToolboxEditor : BaseScript
+	{
+#if ODIN_INSPECTOR
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+#else
+		protected virtual void OnEnable()
+		{
+#endif
+		}
+	}
+}
