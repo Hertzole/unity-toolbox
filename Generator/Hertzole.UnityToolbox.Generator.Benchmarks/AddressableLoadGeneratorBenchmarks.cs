a10b3515fec6f4cf7f7c9779766a16278250cbe6
Hertzole <hampusringstrom@gmail.com>
2023-12-15 19:27:08 +0100

    test: AddressableLoadGenerator benchmarks

---
+++ Generator/Hertzole.UnityToolbox.Generator.Benchmarks/AddressableLoadGeneratorBenchmarks.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using Hertzole.UnityToolbox.Generator.Tests;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Hertzole.UnityToolbox.Generator.Benchmarks;
+
+[MemoryDiagnoser]
+public class AddressableLoadGeneratorBenchmarks
+{
+	private CSharpGeneratorDriver driver = null!;
+	private CSharpCompilation compilation = null!;
+
+	[GlobalSetup]
+	public void GlobalSetup()
+	{
+		AddressableLoadGenerator generator = new AddressableLoadGenerator();
+
+		driver = CSharpGeneratorDriver.Create(generator);
+		
+		const string base_path = "../../../../../../../../../Library/ScriptAssemblies/";
+
+		compilation =
+			CSharpCompilation.Create(nameof(AddressableLoadGeneratorBenchmarks), new[]
+				{
+					CSharpSyntaxTree.ParseText(AddressableLoadGeneratorTests.ADDRESSABLE_LOAD_CLASS_SIMPLE),
+					CSharpSyntaxTree.ParseText(AddressableLoadGeneratorTests.ADDRESSABLE_LOAD_CLASS_SIMPLE_T)
+				},
+				new[]
+				{
+					MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+					MetadataReference.CreateFromFile(base_path + "Hertzole.UnityToolbox.dll"),
+					MetadataReference.CreateFromFile(base_path + "Hertzole.ScriptableValues.dll"),
+					MetadataReference.CreateFromFile(base_path + "Unity.Addressables.dll"),
+					MetadataReference.CreateFromFile(base_path + "Unity.ResourceManager.dll")
+				});
+	}
+
+	[Benchmark]
+	public void Run()
+	{
+		driver.RunGenerators(compilation);
+	}
+}
---
+++ Generator/Hertzole.UnityToolbox.Generator.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+
+namespace Hertzole.UnityToolbox.Generator.Benchmarks;
+
+internal static class Program
+{
+	public static void Main(string[] args)
+	{
+		BenchmarkRunner.Run<AddressableLoadGeneratorBenchmarks>();
+	}
+}
