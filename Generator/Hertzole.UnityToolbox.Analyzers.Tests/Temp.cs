e9a64d92cd41df06ca008eb325a4d31215f2391b
Hertzole <contact@hertzole.se>
2024-11-12 11:12:31 +0100

    ci: hopefully fix build pipeline

---
+++ Generator/Hertzole.UnityToolbox.Analyzers.Tests/Temp.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Hertzole.UnityToolbox.Analyzers.Tests;
+
+// This is just to make sure the assembly gets tested.
+public class Temp
+{
+	[Fact]
+	public void Test()
+	{
+		Assert.True(true);
+	}
+}
