56ce82d941542871b0bcc4ab87f48a92cd472884
Hertzole <contact@hertzole.se>
2024-11-12 16:34:42 +0100

    fix: assembly version not being applied to generators

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.12.0")]
+[assembly: AssemblyFileVersion("1.12.0")]
--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -2,5 +2,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("1.12.0")]
+[assembly: AssemblyFileVersion("1.12.0")]
 
 [assembly: InternalsVisibleTo("Hertzole.UnityToolbox.Editor")]
