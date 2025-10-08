95ea24ff56ec2a33f363d7a191a5f6cffbe8b9ce
Hertzole <contact@hertzole.se>
2025-10-08 19:05:45 +0200

    feat: new WeightedRandom methods using Func to get weight

---
+++ Runtime/Helpers/WeightedRandom.Mathematics.cs
@@ -0,0 +1,56 @@
+#if TOOLBOX_MATHEMATICS
+using System;
+using System.Collections.Generic;
+using Random = Unity.Mathematics.Random;
+
+namespace Hertzole.UnityToolbox
+{
+    partial class WeightedRandom
+    {
+        public static T GetRandom<T>(IReadOnlyList<T> list, Func<T, int> getWeight, ref Random random)
+        {
+            int totalWeight = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                totalWeight += getWeight(list[i]);
+            }
+
+            int randomNumber = random.NextInt(0, totalWeight);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (randomNumber < getWeight(list[i]))
+                {
+                    return list[i];
+                }
+
+                randomNumber -= getWeight(list[i]);
+            }
+
+            return list[list.Count - 1];
+        }
+
+        [Obsolete("Use the overload with the getWeight parameter instead.")]
+        public static T GetRandom<T>(IReadOnlyList<T> list, ref Random random) where T : IWeighted
+        {
+            int totalWeight = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                totalWeight += list[i].RandomWeight;
+            }
+
+            int randomNumber = random.NextInt(0, totalWeight);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (randomNumber < list[i].RandomWeight)
+                {
+                    return list[i];
+                }
+
+                randomNumber -= list[i].RandomWeight;
+            }
+
+            return list[list.Count - 1];
+        }
+    }
+}
+#endif
