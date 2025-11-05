af68b747e652ed90594c7cffdf782a05da64b845
Hertzole <contact@hertzole.se>
2025-11-05 01:03:00 +0100

    refactor: use IReadOnlyX for collection extensions

--- Packages/se.hertzole.unitytoolbox/Runtime/Helpers/Internal/ThrowHelper.cs
+++ Packages/se.hertzole.unitytoolbox/Runtime/Helpers/Internal/ThrowHelper.cs
@@ -9,48 +9,48 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Hertzole.UnityToolbox
 {
-	internal static class ThrowHelper
-	{
-		/// <summary>
-		///     Throws an exception if the given value is null.
-		/// </summary>
-		/// <param name="obj">The value to check.</param>
-		/// <param name="paramName">The name of the parameter.</param>
-		/// <typeparam name="T">The type of the object.</typeparam>
-		/// <exception cref="ArgumentNullException">If the object is null.</exception>
-		[Conditional("DEBUG")]
-		public static void ThrowIfNull<T>(
+    internal static class ThrowHelper
+    {
+        /// <summary>
+        ///     Throws an exception if the given value is null.
+        /// </summary>
+        /// <param name="obj">The value to check.</param>
+        /// <param name="paramName">The name of the parameter.</param>
+        /// <typeparam name="T">The type of the object.</typeparam>
+        /// <exception cref="ArgumentNullException">If the object is null.</exception>
+        [Conditional("DEBUG")]
+        public static void ThrowIfNull<T>(
 #if NETSTANDARD2_1
-			[NotNull]
+            [NotNull]
 #endif
-			T? obj,
-			string paramName) where T : class
-		{
+            T? obj,
+            string paramName) where T : class
+        {
 #if DEBUG
-			// Do some special checking for Unity objects as they override the null check.
-			if ((obj is Object unityObj && unityObj == null) || obj == null)
-			{
-				throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
-			}
+            // Do some special checking for Unity objects as they override the null check.
+            if ((obj is Object unityObj && unityObj == null) || obj == null)
+            {
+                throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
+            }
 #endif // DEBUG
-		}
+        }
 
-		/// <summary>
-		///     Throws an exception if the collection is empty.
-		/// </summary>
-		/// <param name="list">The collection to check.</param>
-		/// <param name="paramName">The name of the collection.</param>
-		/// <typeparam name="T">The type in the collection.</typeparam>
-		/// <exception cref="ArgumentException">If the collection is empty.</exception>
-		[Conditional("DEBUG")]
-		public static void ThrowIfEmpty<T>(ICollection<T> list, string paramName)
-		{
+        /// <summary>
+        ///     Throws an exception if the collection is empty.
+        /// </summary>
+        /// <param name="list">The collection to check.</param>
+        /// <param name="paramName">The name of the collection.</param>
+        /// <typeparam name="T">The type in the collection.</typeparam>
+        /// <exception cref="ArgumentException">If the collection is empty.</exception>
+        [Conditional("DEBUG")]
+        public static void ThrowIfEmpty<T>(IReadOnlyCollection<T> list, string paramName)
+        {
 #if DEBUG
-			if (list.Count == 0)
-			{
-				throw new ArgumentException($"{paramName} cannot be empty.", paramName);
-			}
+            if (list.Count == 0)
+            {
+                throw new ArgumentException($"{paramName} cannot be empty.", paramName);
+            }
 #endif // DEBUG
-		}
-	}
+        }
+    }
 }
